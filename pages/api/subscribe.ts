a47bbced566299b90ea250005760ef4c4b08db63
Saky0 <daviteremattos@gmail.com>
2024-07-05 13:15:10 -0300

    Testing

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -6,7 +6,7 @@ const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 
 const serviceAccountAuth = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY!,
+  key: process.env.GOOGLE_PRIVATE_KEY!.split(String.raw`\n`).join('\n'),
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
