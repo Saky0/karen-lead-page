6ef89cca9c69e197074ff07f453eca84192b54f4
Saky0 <daviteremattos@gmail.com>
2024-07-05 15:11:02 -0300

    Changing PDF

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -6,7 +6,7 @@ export const DownloadButton: React.FC = () => {
     const downloadFile = (url: string) => {
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', 'myfile.pdf'); // Nome do arquivo no download
+        link.setAttribute('download', 'LIBIDO SEM PAUSA.pdf'); // Nome do arquivo no download
         document.body.appendChild(link);
         link.click();
         link.remove();
@@ -14,7 +14,7 @@ export const DownloadButton: React.FC = () => {
 
     return (
         <button  
-            onClick={() => downloadFile('/files/LIBIDO SEM PAUSA_compressed.pdf')}
+            onClick={() => downloadFile('/files/LIBIDO SEM PAUSA.pdf')}
             className={`btn px-5 min-[420px]:px-8 py-4 text-base min-[420px]:text-lg font-extrabold rounded-md uppercase tracking-wider`}
             type="submit">
             Baixar agora
